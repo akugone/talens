18ea150aac276bccb4b1ae4db108a17d781c71d9
Akumaw <dev@akumaw.com>
2023-01-04 17:18:23 +0100

    Add sign in with Lens Add persisting Auth state Authenticated Api request

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,17 @@
+import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
+import useLogin from "../lib/auth/useLogin";
 import {
   PublicationSortCriteria,
   useExplorePublicationsQuery,
 } from "../graphql/generated";
 
 export default function Home() {
-  const { data, isLoading, error } = useExplorePublicationsQuery({
-    request: {
-      sortCriteria: PublicationSortCriteria.TopCollected,
-    },
-  });
-  console.log({
-    data,
-    isLoading,
-    error,
-  });
+  const address = useAddress();
+  const { mutate: requestLogin } = useLogin();
 
-  return <>Hello</>;
+  if (!address) {
+    return <ConnectWallet />;
+  }
+
+  return <button onClick={() => requestLogin()}>Login</button>;
 }
