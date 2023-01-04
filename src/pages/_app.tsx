297a66060513747d00f88dd1c52094fde5f3dd88
Akumaw <dev@akumaw.com>
2023-01-04 15:42:42 +0100

    Add ThirdWeb package

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,19 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThirdwebProvider, ChainId } from "@thirdweb-dev/react";
+
+const desiredChainId = ChainId.Polygon;
 
 export default function App({ Component, pageProps }: AppProps) {
   // Create a client
   const queryClient = new QueryClient();
   return (
     // Provide the client to your App
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
+    <ThirdwebProvider desiredChainId={desiredChainId}>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </ThirdwebProvider>
   );
 }
