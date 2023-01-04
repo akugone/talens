f5ce7ce57c185bbde758e3e58e2d398334a6d13b
Akumaw <dev@akumaw.com>
2023-01-04 23:18:50 +0100

    Add display feed and Lens Logic

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,20 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThirdwebProvider, ChainId } from "@thirdweb-dev/react";
-
-const desiredChainId = ChainId.Polygon;
+import Header from "../components/Header";
 
 export default function App({ Component, pageProps }: AppProps) {
+  // the chainId our app wants to be running on
+  // for our example the Polygon Mumbai Testnet
+  const desiredChainId = ChainId.Polygon;
+
   // Create a client
   const queryClient = new QueryClient();
+
   return (
-    // Provide the client to your App
     <ThirdwebProvider desiredChainId={desiredChainId}>
       <QueryClientProvider client={queryClient}>
+        <Header />
         <Component {...pageProps} />
       </QueryClientProvider>
     </ThirdwebProvider>
